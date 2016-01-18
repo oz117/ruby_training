9e60240c271f234665811477387126124fc16573
paulos_a <paulos_a@epitech.eu>
2016-01-18 18:42:38 +0100

    Added ex 01

---
+++ Ruby_d01/ex_01/my_show_platypus.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/ruby
+
+def my_show_platypus(iterations = 0)
+  iterations.times {puts "Platypus\n"}
+end
+
+my_show_platypus
+my_show_platypus(3)
+my_show_platypus(-3)
+my_show_platypus(0)
