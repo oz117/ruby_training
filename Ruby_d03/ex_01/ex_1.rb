b22904201d67497c11f010f6d10155e5a7ea6246
paulos_a <paulos_a@epitech.eu>
2016-01-20 16:41:02 +0100

    Updated ex_01 from day 3

--- Ruby_d03/ex_01/ex_1.rb
+++ Ruby_d03/ex_01/ex_1.rb
@@ -1,10 +1,8 @@
-#!/usr/bin/ruby
-
-def my_super_inspect(test)
-  arr = Hash.new
-  arr["instance variables"] = test.instance_variables
-  arr["public methods"] = test.public_methods(false)
-  arr["private methods"] = test.private_methods(false)
-  arr["protected methods"] = test.protected_methods(false)
-  arr
+def my_super_inspect(obj)
+  {
+    "instance variables" => obj.instance_variables,
+    "public methods" => obj.public_methods(false),
+    "private methods" => obj.private_methods(false),
+    "protected methods" => obj.protected_methods(false)
+  }
 end
